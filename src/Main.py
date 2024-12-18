df6008ece6f7babe0880f52f0eecfa8dc1bf70b0
Johnny Rosenquist <johnny.r@live.se>
2024-12-18 20:20:34 +0100

    Felmeddelande
    404 om ingen spellista hittas till det temat

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
+from pydantic_core.core_schema import none_schema
+
 from config import spoonacular_api_key, spotify_api_key, spotify_api_secret
 from pydantic import BaseModel
 from requests import post, get
@@ -26,14 +28,19 @@ def set_token():
 token = set_token()
 @app.get("/v1.0/music/playlists/")
 def return_playlist(theme : str):
-    return get_playlist(theme)
+    url = get_playlist(theme)
+    if url is None:
+        raise HTTPException(status_code=404, detail="No playlist matches that theme")
+    return url
 
 def get_playlist(theme : str):
     request_header = {"Authorization" : "Bearer " + token}
     spotify_endpoint = "https://api.spotify.com/v1/search?q="+theme+"&type=playlist&limit=1"
-    response = get(spotify_endpoint, headers=request_header)
-    response_json = json.loads(response.content)["playlists"]["items"]
-    playlist_id = response_json[0]["id"]
+    response = get(spotify_endpoint, headers=request_header).json()
+    response_dict = response["playlists"]["items"]
+    playlist_id = response_dict[0]["id"]
+    if playlist_id is None:
+        return None
     embedded_url = "https://open.spotify.com/embed/playlist/"+playlist_id+"?utm_source=generator"
     return embedded_url
 
